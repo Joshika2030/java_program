ffef08bd197588323252968589289e590bc3a642
Joshika2030 <joshikaanand2007@gmail.com>
2026-09-12 14:49:08 +0530

    Add files via upload

---
+++ anagrams.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class anagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not anagram");
+            return;
+        }
+
+        int[] count = new int[256];
+
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            count[str2.charAt(i)]--;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (count[i] != 0) {
+                System.out.println("Not anagram");
+                return;
+            }
+        }
+
+        System.out.println("Anagram");
+
+    }
+}                                
