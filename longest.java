417b12884076f90ddfeb9a33676dcfcaa60aeef0
Joshika2030 <joshikaanand2007@gmail.com>
2026-09-12 13:51:36 +0530

    Add files via upload

---
+++ longest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class longest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ' ') {
+                if (count > max) {
+                    max = count;
+                }
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+
+        if (count > max) {
+            max = count;
+        }
+
+        System.out.println(max);
+    }
+}
